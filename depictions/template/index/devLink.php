aad77e3d00a78026124b132a56bb529d8b2b9ed2
tonyk99 <Tony@MacBook.local>
2016-11-10 16:49:57 -0500

    testing depictions

--- depictions/template/index/devLink.php
+++
@@ -1,4 +0,0 @@
-<ul>
-    <li>
-        <a class=\"devLink\" target=\"_blank\" href="https://twitter.com/tonykraft1">@ADeveloper on Twitter</a></li>
-    <li>
--- depictions/template/index/footer.php
+++
@@ -1,3 +0,0 @@
-<p><a href="https://iostonykraft.github.io" target="_blank">Tony's Repo</a></p>
-</body>
-</html>
